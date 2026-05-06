0b4769beadd5d4cdac186692d5bb048b43d079ff
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-05-06 12:37:55 +0600

    summer-essentials-store setup is completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// import { ToastContainer } from "react-toastify";
+// import Navber from "@/components/Navber";
+// import Footer from "@/components/Footer";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const outfit = Outfit({
   subsets: ["latin"],
 });
 
@@ -18,11 +16,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" data-theme="light" className={`h-full antialiased`}>
+      <body className={`${outfit.className} min-h-full flex flex-col`}>
+        {/* <Navber /> */}
+        <main className="container mx-auto">{children}</main>
+        {/* <Footer /> */}
+        {/* <ToastContainer /> */}
+      </body>
     </html>
   );
 }
