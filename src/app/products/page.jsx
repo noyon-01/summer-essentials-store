21318a34c54a6813f910eef1a94931538df54819
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-05-06 13:54:05 +0600

    authentication is added in summer essentials store

---
+++ src/app/products/page.jsx
@@ -0,0 +1,19 @@
+import ProductCard from "@/components/ProductCard";
+
+export default async function Products() {
+  const res = await fetch("https://assignment-08-tan.vercel.app/db.json");
+  const data = await res.json();
+
+  return (
+    <div className="max-w-6xl mx-auto my-10">
+      <h1 className="text-4xl font-bold mb-5 text-center md:text-left">
+        All Products
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {data.products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
